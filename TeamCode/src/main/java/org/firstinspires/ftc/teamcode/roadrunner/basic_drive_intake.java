d10d22d87577622ae73fc3fa2f99b2efb403818e
henrywb-arch <henrywb2010@gmail.com>
2026-08-31 17:24:54 -0600

    Make stuff better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/basic_drive_intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/basic_drive_intake.java
@@ -1,8 +1,6 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.roadrunner;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
