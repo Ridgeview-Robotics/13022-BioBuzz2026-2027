116780651c9eba7cc233124f441e163a268dd60d
henrywb-arch <henrywb2010@gmail.com>
2026-09-03 15:59:00 -0600

    refactor some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/basic_drive_intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/basic_drive_intake.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.roadrunner;
+package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
